fc60c6ebc54d3d7f884db84b63ddf2ff38f75b20
Jessica Cayaban <jest.cayaban@gmail.com>
2026-07-31 07:10:56 +0900

    Updated lib function to accept generics

---
+++ src/bin/06.rs
@@ -0,0 +1,18 @@
+use flip_flop::*;
+
+fn main() {
+    let example = open_file("./data/examples/06.txt");
+    let data = open_file("./data/inputs/06.txt");
+    run_day(example, part_1, part_2, part_3);
+    run_day(data, part_1, part_2, part_3);
+}
+
+fn part_1(input: &[&str]) -> usize {
+    0
+}
+fn part_2(input: &[&str]) -> usize {
+    0
+}
+fn part_3(input: &[&str]) -> usize {
+    0
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::fs::read_to_string;
 use std::time::Instant;
 
@@ -9,29 +10,29 @@ pub fn open_file(filename: &str) -> Vec<String> {
         .collect()
 }
 
-pub fn run_day(
+pub fn run_day<T: Debug>(
     file: Vec<String>,
-    part1: fn(&[&str]) -> usize,
-    part2: fn(&[&str]) -> usize,
-    part3: fn(&[&str]) -> usize,
+    part1: fn(&[&str]) -> T,
+    part2: fn(&[&str]) -> T,
+    part3: fn(&[&str]) -> T,
 ) {
     let file_slice: Vec<&str> = file.iter().map(|s| s.as_str()).collect();
 
     let now = Instant::now();
     let p1 = part1(&file_slice);
-    println!("part 1 is {p1}");
+    println!("part 1 is {:?}", p1);
 
     let elapsed = now.elapsed();
     println!("Took {} mils", elapsed.as_millis());
 
     let p2 = part2(&file_slice);
-    println!("Part 2 is {p2}");
+    println!("Part 2 is {:?}", p2);
 
     let elapsed = now.elapsed();
     println!("Took {} mils", elapsed.as_millis());
 
     let p3 = part3(&file_slice);
-    println!("Part 3 is {p3}");
+    println!("Part 3 is {:?}", p3);
 
     let elapsed = now.elapsed();
     println!("Took {} mils", elapsed.as_millis());
